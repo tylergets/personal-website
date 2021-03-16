1e8c7a2f868c3222a5b8a4f0c5f37518cb0376e7
Tyler Getsay <tylergetsay@gmail.com>
2021-03-15 22:24:13 -0400

    chore: index

---
+++ pages/awesome/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      Stuff I like
+    </div>
+    <div>
+      <nuxt-link to="/awesome/podcasts" class="text-blue-500 underline">
+        Podcasts
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
